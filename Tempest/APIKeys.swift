bd05a87c12fd6de9e75f2ec2831a24420ceae2ae
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-10-25 12:41:21 -0500

    Committing with keys set to empty strings

---
+++ Tempest/APIKeys.swift
@@ -0,0 +1,16 @@
+//
+//  APIKeys.swift
+//  Tempest
+//
+//  Created by Matthew Riley on 10/25/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+//Struct to contain our API keys from the DarkSky API and the Google Geocoding API
+
+struct APIKeys {
+    let darkSkyKey = ""
+    let googleKay = ""
+}
