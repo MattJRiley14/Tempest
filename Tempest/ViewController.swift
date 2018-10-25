066101cdf1d2188de9e05d4db03f51587c784f7a
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-10-25 14:08:24 -0500

    Set up API request in ViewController file

--- Tempest/ViewController.swift
+++ Tempest/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let apiKeys = APIKeys()
+        
+        let darkSkyURL = "https://api.darksky.net/forecast/"
+        let darkSkyKey = apiKeys.darkSkyKey
+        let latitude = "37.8267"
+        let longitude = "-122.4233"
+        
+        let url = darkSkyURL + darkSkyKey + "/" + latitude + "," + longitude
+        
+        let request = Alamofire.request(url)
+        
+        //Carrying out our Request
+        request.responseJSON { response in
+            //Switching based on the Result of the Request
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
-
-
 }
 
